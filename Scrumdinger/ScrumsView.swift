8099d2fa4de6156f1dca8bec4ee1da2bf7b53464
Anupama <anupama80286@gmail.com>
2021-12-06 13:18:11 +0530

    initial commit

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,34 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Anupama Biswas on 05/12/21.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List{
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: {}){
+            Image(systemName: "plus")
+        } )
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            ScrumsView(scrums: DailyScrum.data)
+        }
+    }
+}
